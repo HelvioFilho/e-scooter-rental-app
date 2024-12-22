6a96523e08af67466bcb85a94964ff9c77c5ae7a
Helvio Filho <helviosvf@gmail.com>
2024-12-22 20:10:24 -0300

    refactor: clean up unused files and add Map component
    - Removed unnecessary files and components to streamline the project structure:
    - Deleted unused files: prettier.config.js, +html.tsx, +not-found.tsx, details.tsx, Button.tsx, Container.tsx, EditScreenInfo.tsx, ScreenContent.tsx.
    - Updated index.tsx to reflect the cleaned structure.
    - Added a new Map component under src/components for handling map-related functionality.

---
+++ src/components/Map.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Mapbox, { Camera, LocationPuck, MapView } from "@rnmapbox/maps";
+
+Mapbox.setAccessToken(process.env.EXPO_PUBLIC_MAPBOX_TOKEN || "");
+
+export default function Map() {
+  return (
+    <>
+      <MapView style={{ flex: 1 }} styleURL="mapbox://styles/mapbox/dark-v11">
+        <Camera followZoomLevel={16} followUserLocation />
+        <LocationPuck
+          puckBearingEnabled
+          puckBearing="heading"
+          pulsing={{ isEnabled: true }}
+        />
+      </MapView>
+    </>
+  );
+}
