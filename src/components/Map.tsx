50123c2ffcc7fab42b26eee53c0cde6082ed4f32
Helvio Filho <helviosvf@gmail.com>
2024-12-22 21:39:08 -0300

    feat: add clustering and circle overlays for e-scooters on the map
    - Improved map visualization by grouping nearby e-scooters and showing counts for clustered locations:
    - Added clustering to ShapeSource for better scalability with multiple scooters.
    - Implemented CircleLayer to render circle overlays under grouped scooters, indicating the number of e-scooters in each area.
    - Updated SymbolLayer to display the count of scooters within each cluster.
    - Ensured individual e-scooters are represented with custom pin icons for unclustered points.
    - Enhanced user experience with cleaner and more informative map interactions.

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Mapbox, {
   Camera,
+  CircleLayer,
   Images,
   LocationPuck,
   MapView,
@@ -16,7 +17,7 @@ Mapbox.setAccessToken(process.env.EXPO_PUBLIC_MAPBOX_TOKEN || "");
 
 export default function Map() {
   const points = scooters.map((scooter) => point([scooter.long, scooter.lat]));
-  const scootersFeatures = featureCollection(points);
+
   return (
     <>
       <MapView style={{ flex: 1 }} styleURL="mapbox://styles/mapbox/dark-v11">
@@ -26,9 +27,37 @@ export default function Map() {
           puckBearing="heading"
           pulsing={{ isEnabled: true }}
         />
-        <ShapeSource id="scooters" shape={scootersFeatures}>
+        <ShapeSource
+          id="scooters"
+          cluster
+          shape={featureCollection(points)}
+          onPress={(event) => console.log(JSON.stringify(event, null, 2))}
+        >
+          <SymbolLayer
+            id="pointCount"
+            style={{
+              textField: ["get", "point_count"],
+              textSize: 16,
+              textColor: "#ffffff",
+              textPitchAlignment: "map",
+            }}
+          />
+          <CircleLayer
+            id="clusteredScooters"
+            belowLayerID="pointCount"
+            filter={["has", "point_count"]}
+            style={{
+              circlePitchAlignment: "map",
+              circleColor: "#55A4C0",
+              circleRadius: 20,
+              circleOpacity: 0.5,
+              circleStrokeWidth: 1.5,
+              circleStrokeColor: "white",
+            }}
+          />
           <SymbolLayer
             id="symbolLocationSymbols"
+            filter={["!", ["has", "point_count"]]}
             minZoomLevel={1}
             style={{
               iconImage: "pin",
