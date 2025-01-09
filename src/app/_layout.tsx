fc19c5fde254876e007b85fd4a1d3da63098f686
Helvio Filho <helviosvf@gmail.com>
2025-01-09 11:07:25 -0300

    feat: enhance layout with splash screen and font integration
    Updated the layout to:
    - Prevent the splash screen from auto-hiding until fonts are loaded.
    - Integrated Roboto font styles (Regular, Medium, Bold) using expo-font.
    - Improved user experience by ensuring consistent font rendering and smoother splash screen transition.

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,10 +1,36 @@
-import ScooterProvider from "@/providers/ScooterProvider";
+import React, { useEffect } from "react";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_500Medium,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+import * as SplashScreen from "expo-splash-screen";
 import { Stack } from "expo-router";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 
+import ScooterProvider from "@/providers/ScooterProvider";
+
+SplashScreen.preventAutoHideAsync();
 export default function Layout() {
+  const [loaded, error] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_700Bold,
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
+    <GestureHandlerRootView testID="layout-content" style={{ flex: 1 }}>
       <ScooterProvider>
         <Stack />
       </ScooterProvider>
